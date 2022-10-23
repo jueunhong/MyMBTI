aab6fd724e902cce7a896d4c368a2178793d729d
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-10-23 13:39:31 +0900

    Test 화면 답변 선택지 가져오기, 질문 끝나면 Result 화면으로 이동

--- mbti-test/src/pages/Test.js
+++ mbti-test/src/pages/Test.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import styles from "./Test.module.css";
 import {qna, info} from "./data";
+import { useNavigate } from "react-router-dom";
 
 const Test = () => {
     const [question, setQuestion] = useState(qna[0].q);
+    const [answer, setAnswer] = useState([qna[0].a[0].answer, qna[0].a[1].answer, qna[0].a[2].answer]);
     const [num, setNum] = useState(1);
+    const navigate = useNavigate();
     const onClick = () => {
-        setNum(prev => prev+1);
-        setQuestion(qna[num].q);
+        if(num === 12){
+            navigate("/Result");
+        } else{
+            setNum(prev => prev+1);
+            setQuestion(qna[num].q);
+            setAnswer([qna[num].a[0].answer, qna[num].a[1].answer, qna[num].a[2].answer])
+        }
         };
-    
- 
+
     return(
         <div className={styles.Container}>
-          {qna && <div>{question}</div>}
+          {qna &&
+          <>
+            <div>{question}</div>
+            <div>
+                <ul>
+                    {answer.map(a => <li key={a}>{a}</li>)}
+                </ul>
+                
+            </div>
+          </>
+          }
           <button onClick={onClick}><span>다음</span></button>
         </div>
     )
